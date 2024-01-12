6822b7190d9191346113fcbd117c2441fdb19234
FrankGrillo1991 <fsg12491@aol.com>
2024-01-12 05:48:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const notificationsCounter = document.querySelector(".notifications-counter");
+notificationsCounter = document.querySelector(".notifications-counter");
 const markAllButton = document.querySelector(".mark-all-button");
 const notificationDots = document.querySelectorAll(".notification-dot");
 let notifications = document.querySelectorAll("main ul li");
